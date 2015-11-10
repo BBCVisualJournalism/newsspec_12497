6b8bc5cacdb6d792ba2e357cc388594ebb78d8ac
Punit Shah <punit.shah@bbc.co.uk>
2015-11-10 14:59:52 +0000

    added Array.indexOf() polyfill
    for <IE9

--- source/js/facewall.js
+++ source/js/facewall.js
@@ -21,6 +21,29 @@ define(['bootstrap', 'options', 'filters', 'profiles', 'tooltips'], function (ne
         };
     }
 
+    // Array.prototype.indexOf() polyfill
+    // ref: https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Array/indexOf
+    if (!Array.prototype.indexOf) {
+        Array.prototype.indexOf = function (searchElement, fromIndex) {
+            var k;
+            if (this == null) {
+                throw new TypeError('"this" is null or not defined');
+            }
+            var O = Object(this);
+            var len = O.length >>> 0;
+            if (len === 0) { return -1; }
+            var n = +fromIndex || 0;
+            if (Math.abs(n) === Infinity) { n = 0; }
+            if (n >= len) { return -1; }
+            k = Math.max(n >= 0 ? n : len - Math.abs(n), 0);
+            while (k < len) {
+                if (k in O && O[k] === searchElement) { return k; }
+                k++;
+            }
+            return -1;
+        };
+    }
+
     var Facewall = function () {
         this.filters = new Filters();
         this.profiles = new Profiles();
